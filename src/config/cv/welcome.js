631300010f3e6a3c557db6a3d8e3809df703e4e4
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-05-31 23:34:15 +0200

    welcome.json written

--- src/config/cv/welcome.js
+++ src/config/cv/welcome.js
@@ -1,4 +1,14 @@
-export default `
-{
-  "test": "blahblahblah"
+export default `{
+  "hello": "Heya! Welcome on my portfolio. This should show you what I have done and what I can do.",
+  "eeeeeh": [
+    "Ok, maybe it needs a bit of explanation. You can use the file explorer on your left to open files",
+    "These should describe some of the work I have been doing or what studies I did, etc."
+  ],
+  "are-you-mad": "Hehe, no. I just thought it would be an original way to present myself.",
+  "hurry-up": [
+    "Ok if you can't be bothered you can download directly my CV in a PDF format:",
+    "http://vatweb.fr/assets/Vincent_Audebert_CV.pdf",
+    "or click on the PDF icon in the menu bar on the left",
+    "or click on the smiley face in the footer bar on the left",
+  ]
 }`;
