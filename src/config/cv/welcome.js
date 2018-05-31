9fd1e891e2e45c5c2bdd59b33b1ddeb5f406481e
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-06-01 00:08:51 +0200

    rewording welcome

--- src/config/cv/welcome.js
+++ src/config/cv/welcome.js
@@ -1,14 +1,17 @@
 export default `{
-  "hello": "Heya! Welcome on my portfolio. This should show you what I have done and what I can do.",
+  "hello": "Heya! Welcome on my portfolio. It shows you what I have done and what I can do.",
   "eeeeeh": [
     "Ok, maybe it needs a bit of explanation. You can use the file explorer on your left to open files",
-    "These should describe some of the work I have been doing or what studies I did, etc."
+    "These describe some of the work I have been doing or what studies I did, etc."
   ],
-  "are-you-mad": "Hehe, no. I just thought it would be an original way to present myself.",
+  "are-you-mad": [
+    "Hehe, no. I just thought it would be an original way to present myself",
+    "and relevant with my expertise."
+  ],
   "hurry-up": [
     "Ok if you can't be bothered you can download directly my CV in a PDF format:",
     "http://vatweb.fr/assets/Vincent_Audebert_CV.pdf",
     "or click on the PDF icon in the menu bar on the left",
-    "or click on the smiley face in the footer bar on the left",
+    "or click on the smiley face in the footer bar on the left"
   ]
 }`;
