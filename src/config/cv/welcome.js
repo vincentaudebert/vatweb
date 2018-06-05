a3481baa06c007c6017b302c2c9baf5f2d63b416
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-06-05 16:41:06 +0200

    updates welcome json text/format for easier reading on mobile

--- src/config/cv/welcome.js
+++ src/config/cv/welcome.js
@@ -1,21 +1,27 @@
 export default `{
-  "hello": "Heya! Welcome on my portfolio. It shows you what I have done and what I can do.",
-  "eeeeeh": [
-    "Ok, maybe it needs a bit of explanation. You can use the file explorer on your left to open files",
+  "hello": [
+    "Heya! Welcome on my portfolio.", 
+    "It shows you what I have done and what I can do."
+  ],
+  "wait-what": [
+    "Ok, maybe it needs a bit of explanation.",
+    "You can use the file explorer on your left to open files",
     "These describe some of the work I have been doing or what studies I did, etc."
   ],
   "are-you-mad": [
-    "Hehe, no. I just thought it would be an original way to present myself",
-    "and relevant with my expertise."
+    "Hehe, no. I just thought it would be",
+    "an original way to present myself and relevant with my expertise."
   ],
   "aint-nobody-got-time-for-that": [
-    "Ok if you can't be bothered you can download directly my CV in a PDF format:",
+    "Ok if you can't be bothered",
+    "you can download directly my CV in a PDF format:",
     "https://vatweb.fr/assets/Vincent_Audebert_CV.pdf",
-    "or click on the PDF icon in the menu bar on the left",
+    "or click on the PDF icon in the menu bar on the left (desktop only)",
     "or click on the smiley face in the footer bar."
   ],
   "where-is-da-code": [
-    "Oh by the way, I made the source code open source. You can see it here:",
+    "Oh by the way, I made the source code open source.",
+    "You can see it here:",
     "https://github.com/vincentaudebert/vatweb",
     "or click on the git icon in the footer bar.",
   ]
