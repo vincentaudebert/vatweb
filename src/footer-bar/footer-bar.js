6e4f3b190d12ba043bfaabc87f0737a5a9fe00e6
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-06-05 16:15:22 +0200

    new explorer icons + improve render footer bar

--- src/footer-bar/footer-bar.js
+++ src/footer-bar/footer-bar.js
@@ -3,7 +3,7 @@ import GoInfo from 'react-icons/lib/go/info';
 import GoGitBranch from 'react-icons/lib/go/git-branch';
 import GoCloudUpload from 'react-icons/lib/go/cloud-upload';
 import GoCheck from 'react-icons/lib/go/check';
-import GoAlert from 'react-icons/lib/go/alert';
+import MdWarning from 'react-icons/lib/md/warning';
 import FaSmile from 'react-icons/lib/fa/smile-o';
 import FaBell from 'react-icons/lib/fa/bell';
 import TiDelete from 'react-icons/lib/ti/delete';
@@ -19,16 +19,17 @@ const FooterBar = () => (
         title="Download my CV"
         rel="noopener noreferrer"
       >
-        <GoGitBranch /> master (joke)
+        <GoGitBranch className="icon--footer" /> master (joke)
       </a>
       <span className="footer-bar__info--lg">
-        <GoCloudUpload />
+        <GoCloudUpload className="icon--footer" />
       </span>
       <span className="footer-bar__info--lg" title="Active Linters">
-        <GoInfo /> ESLint, Standard
+        <GoInfo className="icon--footer" /> ESLint, Standard
       </span>
       <span className="footer-bar__info--lg" title="0 Warning">
-        <TiDelete /> 0 <GoAlert /> 0
+        <TiDelete className="icon--footer-big" /> 0{' '}
+        <MdWarning className="icon--footer-small" /> 0
       </span>
     </div>
     <div className="d-inline-block right footer-bar--right">
@@ -37,7 +38,7 @@ const FooterBar = () => (
       <span className="footer-bar__info--lg">LF</span>
       <span className="footer-bar__info">JavaScript</span>
       <span className="footer-bar__info">
-        Prettier:<GoCheck />
+        Prettier:<GoCheck className="icon--footer" />
       </span>
       <a
         href="/assets/Vincent_Audebert_CV.pdf"
@@ -46,10 +47,10 @@ const FooterBar = () => (
         title="Download my CV"
         rel="noopener noreferrer"
       >
-        <FaSmile />
+        <FaSmile className="icon--footer" />
       </a>
       <span className="footer-bar__info--lg" role="img" aria-label="Bell">
-        <FaBell />
+        <FaBell className="icon--footer-small" />
       </span>
     </div>
   </div>
---
+++ src/svg/file-svg.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SvgComponent = props => (
+  <svg viewBox="0 0 32 32" {...props}>
+    <path d="M20.938 4.031H7v23.938h10v-2H9V6.031h10v3h4v11h2V7.969l-4.062-3.938zm2.062 18h-2v2h-2v2h2v1.938h2v-2h2v-1.938h-2v-2z" />
+  </svg>
+);
+
+export default SvgComponent;
