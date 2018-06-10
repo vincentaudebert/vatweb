c6fdc44ef93fdd908925d36dd682f6be2b8c95d2
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-06-10 15:13:01 +0200

    adds some config for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import 'raf/polyfill';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 /* eslint-enable import/no-extraneous-dependencies */
