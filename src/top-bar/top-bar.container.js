3a40c79d119533ad8064e8bf85e6f6a305c88801
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-06-10 12:21:32 +0200

    topper tests

--- src/top-bar/top-bar.container.js
+++ src/top-bar/top-bar.container.js
@@ -1,16 +1,14 @@
 // @flow
 import { connect } from 'react-redux';
 import TopBar from './top-bar';
-import type { fileType, fullFileType } from '../app/app.types';
+import type { fullFileType } from '../app/app.types';
 
 type TopBarState = {
   currentFile: fullFileType,
-  openFiles: Array<fileType>,
 };
 
 const mapStateToProps = (state: TopBarState) => ({
   currentFile: state.currentFile,
-  openFiles: state.openFiles,
 });
 
 const mapDispatchToProps = () => ({});
---
+++ src/top-bar/top-bar.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import TopBar from './top-bar';
+
+describe('TopBar', () => {
+  let currentFile;
+  let toggleEditor;
+
+  beforeEach(() => {
+    toggleEditor = jest.fn();
+    currentFile = {
+      name: 'Test Name',
+      content: 'Test Content',
+      inFolder: false,
+      language: 'react',
+      type: 'file',
+    };
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<TopBar currentFile={currentFile} toggleEditor={toggleEditor} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with undefined file name', () => {
+    currentFile.name = undefined;
+    const tree = renderer
+      .create(<TopBar currentFile={currentFile} toggleEditor={toggleEditor} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('close button toggles editor', () => {
+    const topBar = shallow(
+      <TopBar currentFile={currentFile} toggleEditor={toggleEditor} />
+    );
+    topBar.find('button.top-bar__btn--close').simulate('click');
+
+    expect(toggleEditor).toHaveBeenCalled();
+  });
+});
