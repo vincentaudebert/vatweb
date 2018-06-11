6f5301b585db33fb7fff1922d4150ed08b4b2e39
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-06-11 14:14:09 +0200

    fixes indent on education and hobbies file

--- src/config/cv/education.js
+++ src/config/cv/education.js
@@ -2,39 +2,39 @@ export default `import React, { Component } from 'react';
 import type { Props, State } from './education.flow.js';
 
 class Education extends Component<Props, State> {
-    state = {
-        year: 1995,
-    }
+  state = {
+    year: 1995,
+  }
 
-    componentDidUpdate() {
-        const { getDegree } = this.props;
-        const { year } = this.state;
-        if (year === 2007) getDegree('A Levels, Scientific');
-        if (year === 2009) getDegree('University of Paris-Sud — HND Computer Science');
-        if (year === 2012) getDegree(\`University of Paris-Sud — Master's degree of Computer Science and corporate management\`);
-    }
+  componentDidUpdate() {
+    const { getDegree } = this.props;
+    const { year } = this.state;
+    if (year === 2007) getDegree('A Levels, Scientific');
+    if (year === 2009) getDegree('University of Paris-Sud — HND Computer Science');
+    if (year === 2012) getDegree(\`University of Paris-Sud — Master's degree of Computer Science and corporate management\`);
+  }
 
-    componentWillUnmount() {
-        const { travelTo } = this.props;
-        travelTo('New Zealand');
-    }
+  componentWillUnmount() {
+    const { travelTo } = this.props;
+    travelTo('New Zealand');
+  }
 
-    incrementYear = (): void => {
-        this.setState({
-            year: this.state.year + 1;
-        })
-    }
+  incrementYear = (): void => {
+    this.setState({
+      year: this.state.year + 1;
+    })
+  }
 
-    render() {
-        return (
-            <div className="student-life">
-                <p>
-                    Attend lectures, read, write, learn, improve...
-                </p>
-                <button type="button" onClick={this.incrementYear}>Pass Year</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="student-life">
+        <p>
+          Attend lectures, read, write, learn, improve...
+        </p>
+        <button type="button" onClick={this.incrementYear}>Pass Year</button>
+      </div>
+    );
+  }
 }
 
 export default Education;`;
--- src/config/cv/hobbies.js
+++ src/config/cv/hobbies.js
@@ -3,42 +3,42 @@ import type { Props, WeatherProps, State } from './hobbies.flow.js';
 import { random } from 'lodash';
 
 class Hobbies extends Component<Props, State> {
-    static MY_PASSION = 'Paragliding';
-    static outdoorActivities = [
-        'Hiking',
-        'Skiing',
-        'Wakeboarding',
-        'Cycling'
-    ];
-    static indoorActivities = [
-        'Climbing',
-        'Badminton',
-        'Read about Science',
-        'Read about Politics',
-        'Learn about History',
-        'Keep developing and improving HowWindy.com'
-        'Travel somewhere the weather is good',
-    ];
+  static MY_PASSION = 'Paragliding';
+  static outdoorActivities = [
+    'Hiking',
+    'Skiing',
+    'Wakeboarding',
+    'Cycling'
+  ];
+  static indoorActivities = [
+    'Climbing',
+    'Badminton',
+    'Read about Science',
+    'Read about Politics',
+    'Learn about History',
+    'Keep developing and improving HowWindy.com'
+    'Travel somewhere the weather is good',
+  ];
 
-    pickActivity(weather: WeatherProps) {
-        if (weather.good) {
-            if (weather.canFly) return MY_PASSION;
+  pickActivity(weather: WeatherProps) {
+    if (weather.good) {
+      if (weather.canFly) return MY_PASSION;
 
-            return this.outdoorActivities[
-                random(this.outdoorActivities.length - 1)
-            ];
-        }
-        return this.indoorActivities[random(this.indoorActivities.length - 1)];
+      return this.outdoorActivities[
+        random(this.outdoorActivities.length - 1)
+      ];
     }
+    return this.indoorActivities[random(this.indoorActivities.length - 1)];
+  }
 
-    render() {
-        const activity = this.pickActivity(this.props.weather);
-        return (
-            <div className="free-time">
-                <p>I'm going to do {activity}</p>
-            </div>
-        );
-    }
+  render() {
+    const activity = this.pickActivity(this.props.weather);
+    return (
+      <div className="free-time">
+        <p>I'm going to do {activity}</p>
+      </div>
+    );
+  }
 }
 
 export default Hobbies;`;
